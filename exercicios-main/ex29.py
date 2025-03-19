abb98728e2b0412fb0eb9a014ccd25ba1a9864d5
Izael Miranda junior <j.izael@escola.pr.gov.br>
2025-03-19 10:58:46 -0300

    Add files via upload

--- exercicios-main/ex29.py
+++ exercicios-main/ex29.py
@@ -5,10 +5,38 @@ def exibir_menu():
     print("3 - Sair")
     print("-----------------------------")
 
-    def Cadastrar_pessoa (cadastros):
+def Cadastrar_pessoa (cadastros):
         nome = input("nome:")
         idade = input("idade:")
         turma = input("turma:")
         curso = input("curso:")
         cadastros.append({"nome": nome, "idade": idade, "turma": turma, "curso": curso})
         print("Cadastro realizado com sucesso!")
+
+def ver_cadastro (cadastros):
+        if not cadastros:
+            print("nenhum cadastro no sistema")
+        else:
+            print("\n ------ LISTA DE CADASTRO ------")
+                
+            for i, pessoa in enumerate (cadastros, 1):
+                print(f"{i} . nome: {pessoa['Nome']}, idade:{pessoa['Idade']}, turma: {pessoa['Turma']}, curso: {pessoa['Curso']}")  
+
+def main():
+    cadastros = []
+    while True:
+        exibir_menu()
+        opção = input("escolha uma opção:")
+        if opção == "1":
+            Cadastrar_pessoa(cadastros)
+        elif opção == "2":
+            ver_cadastro(cadastros)
+        elif opção == "3":
+            print("obrigado por utilizar o sistema")
+            break
+        else:
+            print("opção inorreta.")
+
+
+if __name__ == "__main__":
+     main()
